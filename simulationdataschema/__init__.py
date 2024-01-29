41cae90c8b692cc6173cced646354e6a2a60dc00
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-01-29 11:24:16 +0100

    Changed name to Simulation
    Improved ModelSystem normalizations

--- simulationdataschema/__init__.py
+++ simulationdataschema/__init__.py
@@ -16,5 +16,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from .general import Computation
-from .model_system import ModelSystem, AtomicCell, Symmetry
+from .general import Simulation
+# from .model_system import ModelSystem, AtomicCell, Symmetry
