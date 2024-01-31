0d0d8c17281b99397436210f802e4e8b9e932e51
ndaelman <ndaelman@physik.hu-berlin.de>
2024-01-31 11:55:39 +0100

    Extend functionalities, as well as path to data

--- utils/hierarchy_handler.py
+++ utils/hierarchy_handler.py
@@ -1,10 +1,17 @@
 from typing import List, Optional, Protocol, Tuple
 import networkx as nx
 
+
 class ISectionInterface(Protocol):
-    def get_name(self) -> str: ...
-    def __lt__(self, other: 'ISectionInterface') -> bool: ...
-    def __gt__(self, other: 'ISectionInterface') -> bool: ...
+    def get_name(self) -> str:
+        ...
+
+    def __lt__(self, other: "ISectionInterface") -> bool:
+        ...
+
+    def __gt__(self, other: "ISectionInterface") -> bool:
+        ...
+
 
 class SectionHandler(ISectionInterface):
     def __init__(self, name: str):
@@ -13,10 +20,10 @@ class SectionHandler(ISectionInterface):
     def get_name(self) -> str:
         return self.name
 
-    def __lt__(self, other: 'SectionHandler') -> bool:
+    def __lt__(self, other: "SectionHandler") -> bool:
         return self.name < other.name
 
-    def __gt__(self, other: 'SectionHandler') -> bool:
+    def __gt__(self, other: "SectionHandler") -> bool:
         return self.name > other.name
 
 
@@ -28,12 +35,12 @@ class HierarchyFactory:
         self.main_graph.add_node("/", data=[])  # Root node with empty section list
 
     def add_node(self, section_handler: ISectionInterface) -> None:
-        '''Add `ISectionInterface` to the temporary graph for later construction.'''
+        """Add `ISectionInterface` to the temporary graph for later construction."""
         node_name = section_handler.get_name()
         self.temp_graph.add_node(node_name, data=section_handler)
 
     def add_path(self, path: str, section_handler: ISectionInterface) -> None:
-        '''Add `ISectionInterface` directly to the main graph under the path.'''
+        """Add `ISectionInterface` directly to the main graph under the path."""
         parts = path.strip("/").split("/")  # clean path and split into parts
         parent = "/"
         for part in parts[:-1]:
@@ -46,7 +53,7 @@ class HierarchyFactory:
         if not self.main_graph.has_node(final_path):
             self.main_graph.add_node(final_path, data=[section_handler])
         else:
-            self.main_graph.nodes[final_path]['data'].append(section_handler)
+            self.main_graph.nodes[final_path]["data"].append(section_handler)
 
     def get_node(self, path: str) -> Optional[nx.DiGraph]:
         if path in self.graph:
@@ -57,8 +64,8 @@ class HierarchyFactory:
             return None
 
     def cd(self, path: str) -> None:
-        '''Change the current path to the target path.
-        All further path specifications are relative to the current path.'''
+        """Change the current path to the target path.
+        All further path specifications are relative to the current path."""
         if path == "/":
             self.current_path = path
             return
@@ -66,31 +73,34 @@ class HierarchyFactory:
             target_path = path
         else:
             target_path = f"{self.current_path}/{path}".replace("//", "/")
-        
+
         if target_path in self.main_graph:
             self.current_path = target_path
         else:
             print(f"Path {target_path} does not exist.")
 
     def pwd(self) -> str:
-        '''Print the current path.'''
+        """Print the current path."""
         return self.current_path
 
     def ls(self, path: Optional[str] = None) -> List[str]:
-        '''List the contents of the target directory (relative path).'''
+        """List the contents of the target directory (relative path)."""
         target_path = path or self.current_path
         if target_path in self.main_graph:
             # List the names of sections in the target directory
-            sections = [handler.get_name() for handler in self.main_graph.nodes[target_path]['data']]
+            sections = [
+                handler.get_name()
+                for handler in self.main_graph.nodes[target_path]["data"]
+            ]
             # List subdirectories
-            subdirs = [node.rsplit('/', 1)[-1] for node in self.main_graph.successors(target_path)]
+            subdirs = [
+                node.rsplit("/", 1)[-1]
+                for node in self.main_graph.successors(target_path)
+            ]
             return sorted(sections + subdirs)
         else:
             print(f"Path {target_path} does not exist.")
             return []
-
-    def __str__(self) -> str:
-        return "\n".join(sorted(nx.lexicographical_topological_sort(self.graph)))
 
     def _match_pattern(self, node: str, pattern_parts: List[str], index: int) -> bool:
         """Recursively checks if the node path matches the pattern."""
@@ -122,3 +132,6 @@ class HierarchyFactory:
             if self._match_pattern(node, pattern_parts, 0):
                 matches.append(node)
         return matches
+
+    def __str__(self) -> str:
+        return "\n".join(sorted(nx.lexicographical_topological_sort(self.graph)))
