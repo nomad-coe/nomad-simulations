642983d4aa2da69347a4117fe9ae325433b0fffc
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-02-09 14:50:23 +0100

    Improved utils function

--- simulationdataschema/utils/utils.py
+++ simulationdataschema/utils/utils.py
@@ -17,40 +17,50 @@
 # limitations under the License.
 #
 
+from typing import Optional
+from structlog.stdlib import BoundLogger
+
 from nomad.datamodel.data import ArchiveSection
 
 
-def get_sub_section_from_section_parent(
-    section: ArchiveSection, sub_section: str, logger
+def get_sibling_section(
+    section: ArchiveSection, sibling_section_name: str, logger: BoundLogger
 ) -> ArchiveSection:
     """
-    Gets the sub_section of a section by performing a seesaw move, going to the parent of
-    section, and then going down to sub_section. Example, if section is `Symmetry`, and we
-    want to resolve `AtomicCell` (sub_section), this methods goes up to `ModelSystem` from `Symmetry`,
-    and then goes down to `AtomicCell`.
+    Gets the sibling section of a section by performing a seesaw move by going to the parent
+    of the section and then going down to the sibling section. This is used, e.g., to get
+    the `AtomicCell` section from the `Symmetry` section and by passing through the `ModelSystem`.
 
-    If the sub_section is a list, it returns the first element of the list. If the sub_section is
-    a single section, it returns the section.
+    Example of the sections structure:
+
+        parent_section
+          |__ section
+          |__ sibling_section
+
+
+    If the sibling_section is a list, it returns the first element of the list. If the sibling_section is
+    a single section, it returns the sibling_section itself.
 
     Args:
-        section (ArchiveSection): The section to check for its parent and retrieve the sub_section.
-        sub_section (str): The name of the sub_section to retrieve from the parent.
+        section (ArchiveSection): The section to check for its parent and retrieve the sibling_section.
+        sibling_section (str): The name of the sibling_section to retrieve from the parent.
+        logger (BoundLogger): The logger to log messages.
 
     Returns:
-        sub_section_sec (ArchiveSection): The sub_section to be returned.
+        sibling_section (ArchiveSection): The sibling_section to be returned.
     """
     if section.m_parent is None:
-        logger.error("Could not find the parent of the section.")
+        logger.warning("Could not find the parent of the section.")
         return
-    if not section.m_parent.m_xpath(sub_section):
-        logger.error("Could not find the section.m_parent.sub_section.")
+    if not section.m_parent.m_xpath(sibling_section_name):
+        logger.warning("Could not find the section.m_parent.sub_section.")
         return
-    sub_section_sec = getattr(section.m_parent, sub_section)
-    if isinstance(sub_section_sec, list):
-        if len(sub_section_sec) == 0:
-            logger.error("The sub_section is empty.")
+    sibling_section = getattr(section.m_parent, sibling_section_name)
+    if isinstance(sibling_section, list):
+        if len(sibling_section) == 0:
+            logger.warning("The sub_section is empty.")
             return
-        return sub_section_sec[0]
-    elif isinstance(sub_section_sec, ArchiveSection):
-        return sub_section_sec
+        return sibling_section[0]  # ? extend for any section targeted as input
+    elif isinstance(sibling_section, ArchiveSection):
+        return sibling_section
     return
